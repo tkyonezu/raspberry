bb5e2c45c35f62440bfd33f407103c348f2c2e44
Sleepy Senzu <tkyonezu@gmail.com>
2017-08-06 00:48:37 +0900

    Change docker-engine to docker-ce.

--- rpi-setup/install-docker.sh
+++ rpi-setup/install-docker.sh
@@ -30,16 +30,20 @@ cat <<EOR >/etc/apt-sources.list.d/docker.list
 deb [arch=armhf] https://download.docker.com/linux/debian $(lsb_release -cs) stable
 EOF
 
-# Docker-Engine
-curl -fsSL https://apt.dockerproject.org/gpg | apt-key add -
-
-cat <<EOF >>/etc/apt/sources.list.d/docker.list
-deb [arch=armhf] https://apt.dockerproject.org/repo raspbian-jessie main
-#deb [arch=armhf] https://apt.dockerproject.org/repo debian-stretch main
-EOF
-
 apt update
 
-apt install -y docker-engine
+apt install -y docker-ce
+
+## # Docker-Engine
+## curl -fsSL https://apt.dockerproject.org/gpg | apt-key add -
+## 
+## cat <<EOF >>/etc/apt/sources.list.d/docker.list
+## deb [arch=armhf] https://apt.dockerproject.org/repo raspbian-jessie main
+## #deb [arch=armhf] https://apt.dockerproject.org/repo debian-stretch main
+## EOF
+## 
+## apt update
+## 
+## apt install -y docker-engine
 
 exit 0
