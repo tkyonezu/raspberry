2b5aff203209a9c30dae055b9622307e05ef3080
Sleepy Senzu <tkyonezu@gmail.com>
2020-09-08 00:04:36 +0900

    alpine version Upgrade to 3.12.0

--- alpine/build.sh
+++ alpine/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=3.8.2
+VERSION=3.12.0
 
 sudo docker build -t armhf/alpine:${VERSION} .
 
--- alpine/setup.sh
+++ alpine/setup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-MAJOR_VERSION=v3.8
-VERSION=3.8.2
+MAJOR_VERSION=v3.12
+VERSION=3.12.0
+ARC=armhf
 
 [ -d rootfs ] || mkdir rootfs
 
-curl -fsSL https://dl-4.alpinelinux.org/alpine/${MAJOR_VERSION}/releases/armhf/alpine-minirootfs-${VERSION}-armhf.tar.gz | sudo tar -C rootfs -zxvf -
+curl -fsSL https://dl-4.alpinelinux.org/alpine/${MAJOR_VERSION}/releases/${ARC}/alpine-minirootfs-${VERSION}-${ARC}.tar.gz | sudo tar -C rootfs -zxvf -
 
 exit 0
