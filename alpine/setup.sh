6c8e6dc22600360fa8e2ad60f5a238857185c98c
Sleepy Senzu <tkyonezu@gmail.com>
2020-09-08 00:13:01 +0900

    Support aarch64 for Raspberry Pi OS 64bit

--- alpine/setup.sh
+++ alpine/setup.sh
@@ -4,6 +4,10 @@ MAJOR_VERSION=v3.12
 VERSION=3.12.0
 ARC=armhf
 
+if [ $(uname -m)" = "aarch64" ]; then
+  ARC=aarch64
+fi
+
 [ -d rootfs ] || mkdir rootfs
 
 curl -fsSL https://dl-4.alpinelinux.org/alpine/${MAJOR_VERSION}/releases/${ARC}/alpine-minirootfs-${VERSION}-${ARC}.tar.gz | sudo tar -C rootfs -zxvf -
