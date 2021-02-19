081a2dc48ed55214599e74e50e11f871ec6779bb
Takeshi Yonezu <tkyonezu@gmail.com>
2021-02-19 16:46:02 +0900

    Support '-f' opthon to output CSV file, or display using by watch

--- rpi-setup/rpi-clock.sh
+++ rpi-setup/rpi-clock.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-watch -n 1 vcgencmd measure_clock arm
+FILE=0
+
+if [ $# -ge 1 ]; then
+  if [ "$1" = "-f" ]; then
+    FILE=1
+  fi
+fi
+
+if [ ${FILE} -eq 1 ]; then
+  while :; do echo $(date +"%Y%m%d %T")','$(vcgencmd measure_clock arm |  sed "s/^.*=//") | tee -a clock.armcsv; sleep 1; done
+else
+  watch -n 1 vcgencmd measure_clock arm
+fi
 
 exit 0
--- rpi-setup/rpi-temp.sh
+++ rpi-setup/rpi-temp.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-while :; do echo `date +"%Y/%m/%d %T"`','`vcgencmd measure_temp | tail -c +6 | sed "s/'C//g"` | tee temperatures.csv; sleep 1; done
+FILE=0
+
+if [ $# -ge 1 ]; then
+  if [ "$1" = "-f" ]; then
+    FILE=1
+  fi
+fi
+
+if [ ${FILE} -eq 1 ]; then
+  while :; do echo $(date +"%Y%m%d %T")','$(vcgencmd measure_temp | tail -c +6 | sed "s/'C$//") | tee -a temperatures.csv; sleep 1; done
+else
+  watch -n 1 vcgencmd measure_temp
+fi
+
 
 exit 0
