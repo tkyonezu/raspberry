64fc4f53e5c05972230d1293b6c5196985eebc3e
Takeshi Yonezu <tkyonezu@gmail.com>
2021-02-19 16:56:25 +0900

    Support CSV file output or display only

--- rpi-setup/rpi-stat.sh
+++ rpi-setup/rpi-stat.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-function pi-stat {
-  HOST=$1
-
-  if [ "${HOST}" != "localhost" ]; then
-    RHOST="ssh ${HOST}"
-  fi
-
+pi-stat() {
   TEMP=$(${RHOST} vcgencmd measure_temp | sed "s/^.*=//")
   CLOCK=$(${RHOST} vcgencmd measure_clock arm | sed "s/^.*=//")
   VOLTS=$(${RHOST} vcgencmd measure_volts | sed "s/^.*=//")
 
-  if [ "${HOST}" = "localhost" ]; then
-    echo "$(hostname): ${TEMP} ${CLOCK} ${VOLTS}"
-  else
-    echo "${HOST}: ${TEMP} ${CLOCK} ${VOLTS}"
-  fi
+  echo "$(date +"%Y%m%d %T"): ${TEMP} ${CLOCK} ${VOLTS}"
 }
 
-pi-stat localhost
+FILE=0
+
+if [ $# -ge 1 ]; then
+  if [ "$1" = "-f" ]; then
+    FILE=1
+  fi
+fi
+
+if [ ${FILE} -eq 1 ]; then
+  while :; do pi-stat | tee -a rpi-stat.csv; sleep 1; done
+else
+  while :; do pi-stat; sleep 1; done
+fi
 
 exit 0
